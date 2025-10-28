a043e384b4c767a9fab39f7d1ff7b9c8c7416ac7
Den4ik3090 <grishaevds@yandex.ru>
2025-10-28 16:05:29 +0300

    npm install -D style-loader css-loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-import { greetingUser } from "./script";
+/* Импортируем переменные из других файлов  и зависитт от них */
+import { greetingUser } from "./script.js";
+console.log(greetingUser);
--- src/script.js
+++ src/script.js
@@ -1,1 +1,2 @@
+/* Файл где создается код Export */
 export const greetingUser = 'Welcome to our restaurant page!';
