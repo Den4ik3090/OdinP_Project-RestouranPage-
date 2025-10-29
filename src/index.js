8dc2411a0e93d852840ddf5545c13d75f1cc6d1a
Den4ik3090 <grishaevds@yandex.ru>
2025-10-29 08:54:50 +0300

    29/10/2025

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 /* Импортируем переменные из других файлов  и зависитт от них */
 import { greetingUser } from "./script.js";
 console.log(greetingUser);
+/* Импортируем стили Сброса CSS */
+import './reset_css.css';
+/* Импортируем основные стили */
 import './style.css';
+
+
